5ced2dfd1ad0283489455af9c0c511d4de57b8ca
HarshaSanaka <sharshavardhan105@gmail.com>
2026-07-18 22:56:55 +0530

    Configure Django Project for vercel deployment

--- backend/voting_backend/settings.py
+++ backend/voting_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)q5j6^+#*x=@rn)oximb%$cm=mb$r8ygos!8ly_p-^^9s22_cw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
